c2b783cb7fa7219a558dc2405ee7e847b36d6228
jgfurlan_ <jgfurlan@github.com>
2024-02-28 22:43:43 -0300

    first run on the pipeline ok

---
+++ run_pipeline.py
@@ -0,0 +1,7 @@
+from pipelines.training_pipeline import train_pipeline
+
+if __name__ == "__main__":
+    """
+    run the pipeline
+    """
+    train_pipeline(data_path="/home/jgfurlan/Documentos/DS/projects/Customer-Satisfaction-Insights/data/olist_customers_dataset.csv")
---
+++ steps/model_train.py
@@ -0,0 +1,15 @@
+import logging
+
+import pandas as pd
+from zenml import step
+
+@step
+def train_model(df: pd.DataFrame) ->  None:
+    """
+    trains the model on the ingested data
+
+    Args:
+        df: the ingested data
+    """
+
+    pass
